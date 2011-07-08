37a9a95fc11a33af7e3faf49f752ece6e2b2fb9d
Scott Olsen <solsen300@gmail.com>
2011-07-08 13:31:02 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FlatTireLounge::Application.config.secret_token = 'eb3e1ef7bcfefb660a5397319b1deca6fc819814adb8e71aaabe3b44ab775e4192438f74d5bee2b56b63e02315a099074b8600f45a3e7d0efcf4f1b27a7b641d'
